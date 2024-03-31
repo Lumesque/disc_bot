80361ee823ffa8bcda1d84803479540521744f06
Joshua Luckie <luckiejoshu@gmail.com>
2024-03-31 12:13:53 -0400

    Fixing typo and adding small bit of code in order to have servers be global

--- disc_bot/server_data/__init__.py
+++ disc_bot/server_data/__init__.py
@@ -8,4 +8,6 @@ history = Path(here, "history.json")
 if history.exists():
     with history.open(mode="r") as f:
         history_data = json.load(f)
-    servers = ServerHolder.from_history(history_data)
+    servers = ServerContainer.from_history(history_data)
+else:
+    servers = ServerContainer()
